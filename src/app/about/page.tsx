8459a4d178d18f1a003d9bc2d4f39a6d412b3f6a
ZeberMVP <rubeen722@gmail.com>
2023-06-16 15:48:40 +0200

    feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,61 @@
+import LargeHeading from "@/components/ui/LargeHeading";
+import Paragraph from "@/components/ui/Paragraph";
+
 export default function Page() {
   return (
-    <div>
-      <main className="min-h-[100rem] bg-white" />
-    </div>
+    <main className="py-8 md:py-12 lg:py-16 w-10/12 md:w-9/12 lg:w-7/12 xl:w-6/12 mx-auto space-y-8 xl:space-y-12">
+      <LargeHeading>WELCOME TO ARTIC BLOOM</LargeHeading>
+      <Paragraph className="text-left">
+        Artic Bloom is a vibrant online platform dedicated to exploring the
+        ever-evolving world of technology and its profound impact on various
+        aspects of our lives. We are a team of passionate writers, tech
+        enthusiasts, and industry experts who are driven by a shared curiosity
+        and a desire to delve deeper into the realms of innovation, creativity,
+        and digital transformation.
+      </Paragraph>
+      <Paragraph className="text-left">
+        Our mission is to provide insightful, thought-provoking, and engaging
+        content that inspires, educates, and empowers our readers. Through
+        in-depth articles, expert analysis, captivating stories, and thought
+        leadership pieces, we aim to demystify complex concepts, highlight
+        emerging trends, and showcase the incredible possibilities that
+        technology brings.
+      </Paragraph>
+      <Paragraph className="text-left">
+        At Artic Bloom, we cover a wide range of topics, including robotics,
+        artificial intelligence, virtual reality, sustainable technology, smart
+        homes, education technology, and more. Our team of writers and
+        contributors bring diverse perspectives and expertise, ensuring that our
+        content is comprehensive, well-researched, and relevant to the rapidly
+        evolving tech landscape.
+      </Paragraph>
+      <Paragraph className="text-left">
+        We believe that technology is not just a tool, but a powerful catalyst
+        for positive change. It has the potential to transform industries,
+        revolutionize the way we live and work, and create a more connected and
+        sustainable future. Through our articles, we aim to foster a deeper
+        understanding of these transformative technologies and their
+        implications, while also addressing the ethical, social, and
+        environmental considerations that arise.
+      </Paragraph>
+      <Paragraph className="text-left">
+        Whether you&apos;re a tech enthusiast, a student, a professional, or
+        simply someone with a curious mind, Artic Bloom is your go-to resource
+        for staying informed, inspired, and empowered in the digital age. Join
+        us on this exciting journey as we navigate the frontiers of technology,
+        explore cutting-edge innovations, and spark meaningful conversations.
+      </Paragraph>
+      <Paragraph className="text-left">
+        We invite you to dive into our articles, engage with our community, and
+        be part of the ever-growing bloom of knowledge and ideas at Artic Bloom.
+        Together, let&apos;s embrace the possibilities, challenge the status
+        quo, and shape a future that harnesses the full potential of technology
+        for the betterment of humanity.
+      </Paragraph>
+      <Paragraph className="text-left">
+        Step into the world of Artic Bloom, where technology meets creativity,
+        and ideas blossom into transformative realities.
+      </Paragraph>
+    </main>
   );
 }
